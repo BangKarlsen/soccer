7853ef8758863db018e221351c8b7fa8d770b63d
bangkarlsen <jhpe04@gmail.com>
2017-02-01 16:24:46 +0000

    use dir and speed to navigate players

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
-function Team(name) {
+function Team(name, side) {
     this.name = name;
-    console.log('Create team: ' + name);
+    this.side = side;
+    console.log('Created ' + side + ' team: ' + name);
 }
 
-Team.prototype.init = function(players, side) {
-    console.log('building up team ' + this.name);
-    // setup our initial team formation
-    for(var i = 0; i < 4; i++) {
-        players[i].x = side === 'left' ? 100 : 450;
-        players[i].y = 400 / (i+1);
-    }
-};
-
-Team.prototype.tick = function(players, opponents, ball) {
-    players.forEach(function(player) {
-        player.x += Math.random() - 0.1;
-        player.y += Math.random() - 0.5;
+Team.prototype.tick = function(playersPos, opponents, ball) {
+    var playerDirs = [];
+    playersPos.forEach(function(player) {
+        // ignore positions and just run
+        playerDirs.push({
+            // runDir: Math.random() * Math.PI * 2,
+            runDir: 0,
+            runSpeed: Math.random() * 3
+        });
     });
+    return playerDirs;
 }
 
-function Player() {
-    this.x = 0;
-    this.y = 0;
-    this.dir = 0;
-};
 
 function Soccer() {
     var canvas = document.getElementById('field');
@@ -41,10 +34,10 @@ function Soccer() {
         h: 90
     };
 
-    this.team1 = new Team('1');
-    this.team2 = new Team('2');
-    this.playersTeam1 = [new Player(), new Player(), new Player(), new Player()];
-    this.playersTeam2 = [new Player(), new Player(), new Player(), new Player()];
+    this.team1 = new Team('1', 'left');
+    this.team2 = new Team('2', 'right');
+    this.playersPosTeam1 = [{x: 200, y: 100}, {x: 200, y: 200}, {x: 200, y: 300}, {x: 200, y: 350}];
+    this.playersPosTeam2 = [{x: 500, y: 100}, {x: 500, y: 200}, {x: 500, y: 300}, {x: 500, y: 350}];
 };
 
 Soccer.prototype.draw = function() {
@@ -52,17 +45,17 @@ Soccer.prototype.draw = function() {
     var goal = this.goal;
     var ctx = this.ctx;
     ctx.fillStyle = 'green';
-    ctx.fillRect(0, 0, field.wPad, field.hPad);                 // green grass
+    ctx.fillRect(0, 0, field.wPad, field.hPad);                     // green grass
     ctx.strokeStyle = 'white';
     ctx.strokeRect(field.x, field.y, field.w, field.h);             // field outline
     ctx.strokeRect(field.x + field.w/2 - 1, field.y, 2, field.h);   // center line
     ctx.strokeRect(field.x, (field.hPad)/2 - goal.h/2, goal.w, goal.h);  // left goal
     ctx.strokeRect(field.w + field.x - goal.w, (field.hPad)/2 - goal.h/2, goal.w, goal.h);  // right goal
 
-    this.playersTeam1.forEach(drawPlayer);
-    this.playersTeam2.forEach(drawPlayer);
+    this.playersPosTeam1.forEach(drawPlayerPos);
+    this.playersPosTeam2.forEach(drawPlayerPos);
 
-    function drawPlayer(player) {
+    function drawPlayerPos(player) {
         ctx.fillStyle = 'white';
         ctx.beginPath();
         ctx.arc(player.x, player.y, 5, 0, Math.PI*2);
@@ -70,23 +63,21 @@ Soccer.prototype.draw = function() {
     }
 };
 
-Soccer.prototype.init = function() {
-    this.team1.init(this.playersTeam1, 'left');
-    this.team2.init(this.playersTeam2, 'right');
-    this.draw();
-}
-
 Soccer.prototype.tick = function(time) {
-    // console.log(time);
-    // var players = team.tick(players, opponents, ball);
-    this.team1.tick(this.playersTeam1, this.playersTeam2);
+    var posTeam1 = this.playersPosTeam1;
+    var playerDirsTeam1 = this.team1.tick(this.playersPosTeam1, this.playersPosTeam2);
+    var i = 0;
+    playerDirsTeam1.forEach(function(playerDir) {
+        posTeam1[i].x += Math.cos(-playerDir.runDir) * playerDir.runSpeed;
+        posTeam1[i].y += Math.sin(-playerDir.runDir) * playerDir.runSpeed;
+        i++;
+    });
     this.draw();
 };
 
 var game = new Soccer();
-game.init();
-
 var start;
+
 function run(timestamp) {
     var refreshRate = 10; // millis
 
