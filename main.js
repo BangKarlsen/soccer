349c6a55592240541329b6e57dbc6c84e1569b91
bangkarlsen <jhpe04@gmail.com>
2017-02-02 11:20:57 +0000

    Draw ball

--- main.js
+++ main.js
@@ -14,13 +14,19 @@ function Soccer() {
         w: 40, 
         h: 90
     };
+    this.ball = {
+        x: this.field.w/2,
+        y: this.field.h/2,
+        dir: 0,
+        speed: 0
+    };
 
     // Find a better way to instatiate teams... hm.
     var t1 = createFcMowgli();
     var t2 = createFcKogle();
     this.team1 = new t1('1', 'left');
     this.team2 = new t2('2', 'right');
-    
+
     this.playersPosTeam1 = [{x: 200, y: 100}, {x: 200, y: 200}, {x: 200, y: 300}, {x: 200, y: 350}];
     this.playersPosTeam2 = [{x: 500, y: 100}, {x: 500, y: 200}, {x: 500, y: 300}, {x: 500, y: 350}];
 };
@@ -28,24 +34,41 @@ function Soccer() {
 Soccer.prototype.draw = function() {
     var field = this.field;
     var goal = this.goal;
+    var ball = this.ball;
     var ctx = this.ctx;
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, 0, field.wPad, field.hPad);                     // green grass
-    ctx.strokeStyle = 'white';
-    ctx.strokeRect(field.x, field.y, field.w, field.h);             // field outline
-    ctx.strokeRect(field.x + field.w/2 - 1, field.y, 2, field.h);   // center line
-    ctx.strokeRect(field.x, (field.hPad)/2 - goal.h/2, goal.w, goal.h);  // left goal
-    ctx.strokeRect(field.w + field.x - goal.w, (field.hPad)/2 - goal.h/2, goal.w, goal.h);  // right goal
-
-    this.playersPosTeam1.forEach(drawPlayerPos);
-    this.playersPosTeam2.forEach(drawPlayerPos);
+    
+    function drawField() {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(0, 0, field.wPad, field.hPad);                          // green grass
+        ctx.strokeStyle = 'white';
+        ctx.strokeRect(field.x, field.y, field.w, field.h);                  // field outline
+        ctx.strokeRect(field.x + field.w/2 - 1, field.y, 2, field.h);        // center line
+        ctx.strokeRect(field.x, (field.hPad)/2 - goal.h/2, goal.w, goal.h);  // left goal
+        ctx.strokeRect(field.w + field.x - goal.w, (field.hPad)/2 - goal.h/2, goal.w, goal.h);  // right goal
+    }
+    
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(ball.x, ball.y, 7, 0, Math.PI*2);
+        ctx.fillStyle = 'grey';
+        ctx.fill();
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+    }
 
     function drawPlayerPos(player) {
-        ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.arc(player.x, player.y, 5, 0, Math.PI*2);
+        ctx.arc(player.x, player.y, 10, 0, Math.PI*2);
+        ctx.fillStyle = 'white';
         ctx.fill();
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
     }
+
+    drawField();
+    drawBall();
+    this.playersPosTeam1.forEach(drawPlayerPos);
+    this.playersPosTeam2.forEach(drawPlayerPos);
 };
 
 Soccer.prototype.tick = function(time) {
@@ -55,12 +78,14 @@ Soccer.prototype.tick = function(time) {
         posTeam1[index].x += Math.cos(-playerDir.runDir) * playerDir.runSpeed; // consider integrating over time
         posTeam1[index].y += Math.sin(-playerDir.runDir) * playerDir.runSpeed; // to be independent of refreshRate
     });
+
     var posTeam2 = this.playersPosTeam2;
     var playerDirsTeam2 = this.team2.tick(this.playersPosTeam2, this.playersPosTeam1);
     playerDirsTeam2.forEach(function(playerDir, index) {
         posTeam2[index].x += Math.cos(-playerDir.runDir) * playerDir.runSpeed; // consider integrating over time
         posTeam2[index].y += Math.sin(-playerDir.runDir) * playerDir.runSpeed; // to be independent of refreshRate
     });
+    
     this.draw();
 };
 
