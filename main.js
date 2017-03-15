9bea9f442c92d910fce14af13c89bab47c6b4744
bangkarlsen <jhpe04@gmail.com>
2017-03-15 20:29:59 +0100

    dont make new dates all the time

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
-
-var lastTime = 0;
-
-function run() {
-    var timeNow = new Date().getTime();
-    if (lastTime != 0) {
-        var elapsed = timeNow - lastTime;
-
-        // xRot += (xSpeed * elapsed) / 1000.0;
-        // yRot += (ySpeed * elapsed) / 1000.0;
-        game.tick(elapsed);
+(function () {
+    function run(timeNow) {
+        if(timeNow) {
+            var elapsed = timeNow - lastTime;
+            lastTime = timeNow;
+            game.tick(elapsed);
+        }
+        requestAnimationFrame(run);
     }
-    lastTime = timeNow;
-    requestAnimationFrame(run);
-}
 
-function createNewGame() {
-    var leftTeam = eval(leftTeamSelect.value)();    // Yikes, it's the evil eval!
-    var rightTeam = eval(rightTeamSelect.value)();  // Be careful not to eval user-submitted input..
-    game = new Soccer(leftTeam, rightTeam);
-}
+    function createNewGame() {
+        var leftTeam = eval(leftTeamSelect.value)();    // Yikes, it's the evil eval!
+        var rightTeam = eval(rightTeamSelect.value)();  // Be careful not to eval user-submitted input..
+        game = new Soccer(leftTeam, rightTeam);
+    }
 
-var game;
-var leftTeamSelect = document.getElementById('left-team');
-var rightTeamSelect = document.getElementById('right-team');
-leftTeamSelect.addEventListener('change', createNewGame);
-rightTeamSelect.addEventListener('change', createNewGame);
-createNewGame();
-run();
+    var lastTime = 0;
+    var game;
+    var leftTeamSelect = document.getElementById('left-team');
+    var rightTeamSelect = document.getElementById('right-team');
+    leftTeamSelect.addEventListener('change', createNewGame);
+    rightTeamSelect.addEventListener('change', createNewGame);
+    
+    createNewGame();
+    run();
+}())
